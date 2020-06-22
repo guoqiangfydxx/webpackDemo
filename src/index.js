0526ca9e92d916b6dc60e6ecd0d4cb725f10c1a1
guoqiang <18852852576@163.com>
2020-06-22 23:35:04 +0800

    feat: 处理各种常见的资源

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import _ from 'loadsh'
+import './index.css'
+import Logo from './assets/logo.jpg'
+import XmlData from './data.xml'
+
 function component() {
   var element = document.createElement("div");
+  element.className = 'hello'
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  var myIcon = new Image()
+  myIcon.src = Logo
+  element.appendChild(myIcon)
+
+  console.log(XmlData)
 
   return element;
 }
